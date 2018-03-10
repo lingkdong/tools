9f2cdca01742d88528093fc0bef5074a572a8971
lkdong <lingkdong@outlook.com>
2018-03-10 22:18:06 +0800

    delete target and idea

--- target/classes/static/js/open.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    var pre=$("#pre");
-    var type=$(pre).attr("data-type");
-    $(pre).addClass("prettyprint");
-    if(type)$(pre).addClass("lang-"+type);
-    prettyPrint();
-})
--- target/classes/static/js/require-config.js
+++
@@ -1,7 +0,0 @@
-require.config({
-    paths: {
-        "beautify-css": "/js/format/beautify-css" ,
-        "beautify-html": "/js/format/beautify-html" ,
-        "beautify": "/js/format/beautify",
-    }
-});
