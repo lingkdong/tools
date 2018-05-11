f2674419d01cf5255c86b8313c7cd63fcd680f7b
lkdong <lingkdong@outlook.com>
2018-05-12 02:10:57 +0800

    del

--- target/classes/static/js/require-config.js
+++
@@ -1,7 +0,0 @@
-require.config({
-    paths: {
-        "beautify-css": "/tools/js/format/beautify-css" ,
-        "beautify-html": "/tools/js/format/beautify-html" ,
-        "beautify": "/tools/js/format/beautify",
-    }
-});
